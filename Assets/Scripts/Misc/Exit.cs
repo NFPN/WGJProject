0dfa1f22cd06222456bcf4af3cfa96d4afb9721a
Natanael Felipe Pereira Nunes <natanaelfpnunes@gmail.com>
2018-01-31 20:08:31 -0300

    Finishing stuff left over/ polishing a little

--- Assets/Scripts/Misc/Exit.cs
+++ Assets/Scripts/Misc/Exit.cs
@@ -5,6 +5,7 @@ public class Exit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log(1);
         if(collision.gameObject.tag == "Player")
         {
             GameObject.Find("SceneLevelManager").GetComponent<ChangeScene>().SceneToChangeTo(SceneManager.GetActiveScene().buildIndex + 1);
